7a7fcd2b5f02bddb429ba15f47c6ffc2fe03359a
Roberto Chávez Trujillo <38190230+Rchatru@users.noreply.github.com>
2023-07-20 12:23:09 +0100

    reverted changes

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
-class Config:
-    env_prefix = "profile_"
 
 # Web App Title
 st.markdown('''
